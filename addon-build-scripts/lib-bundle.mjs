24ca3d2caf54d6020dbcb58d1b05401feaff164b
ericchase <github@eric.addymail.com>
2024-06-08 19:58:17 -0500

    updated bundle code
    Signed-off-by: ericchase <github@eric.addymail.com>

--- addon-build-scripts/lib-bundle.mjs
+++ addon-build-scripts/lib-bundle.mjs
@@ -1,7 +1,6 @@
-import archiver from 'archiver';
-import node_fs from 'node:fs';
+import node_fs from 'node:fs/promises';
 
-import { Config, createDirectory, toSnakeCase } from './lib.mjs';
+import { Config, createDirectory, run, toSnakeCase } from './lib.mjs';
 
 /**
  * @param {string} browser
@@ -9,28 +8,7 @@ import { Config, createDirectory, toSnakeCase } from './lib.mjs';
  */
 export async function bundle(browser, manifest) {
   await createDirectory(`./release/${browser}`);
-  // create archive
-  const archive = archiver('zip', {
-    zlib: { level: 9 },
-  });
-  archive.on('warning', function (err) {
-    if (err.code === 'ENOENT') {
-      console.log(err);
-    } else {
-      throw err;
-    }
-  });
-  archive.on('error', function (err) {
-    throw err;
-  });
-  const output = node_fs.createWriteStream(`./release/${browser}/${toSnakeCase(manifest.get('name'))}-v${manifest.get('version')}.zip`);
-  output.on('close', function () {
-    console.log(`${browser}:`, archive.pointer() + ' total bytes');
-  });
-  output.on('end', function () {
-    console.log('Data has been drained');
-  });
-  archive.pipe(output);
-  archive.directory(`./build/${browser}`, false);
-  await archive.finalize();
+  const archive_name = `${toSnakeCase(manifest.get('name'))}-v${manifest.get('version')}.zip`;
+  await node_fs.rm(`./release/${browser}/${archive_name}`);
+  run('7z', ['a', '-tzip', `./release/${browser}/${archive_name}`, `./build/${browser}/*`]);
 }
